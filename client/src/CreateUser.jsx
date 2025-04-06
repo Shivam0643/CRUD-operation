3fe3ea8c6131ad95c9e3b113888e593b14a494bf
Shivam0643 <shivamlashkari246@gmail.com>
2025-04-06 17:20:22 +0530

    Done

---
+++ client/src/CreateUser.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const CreateUser = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [age, setAge] = useState();
+  const navigate = useNavigate();
+
+  const sumbit = (e) => {
+    e.preventDefault();
+    // Add User to database
+    axios
+      .post("http://localhost:3001/CreateUser", { name, email, age })
+      .then((result) => {
+        console.log(result);
+        navigate("/"); // Redirect to home page after user creation
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <form onSubmit={sumbit}>
+          <h2>Add User</h2>
+          <div className="mb-2">
+            <label htmlFor="">Name</label>
+            <input
+              type="text"
+              placeholder="Enter Name"
+              className="form-control"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="">Email</label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              className="form-control"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="">Age</label>
+            <input
+              type="text"
+              placeholder="Enter Age"
+              className="form-control"
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
+          <button className="btn btn-success">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateUser;
